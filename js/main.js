7e19273bdee4c0eecf14a8431643684a9de3c5e0
montez-french <darrellmfrench@gmail.com>
2017-10-09 12:34:12 -0700

    Update to fix resize issue

--- js/main.js
+++ js/main.js
@@ -8,13 +8,13 @@ $carousel.flickity({
   adaptiveHeight: true,
   resize: true,
   autoPlay: true,
+  setGallerySize: false,
   wrapAround: true
 });
 
 $carouselCellLink.on('click', function () {
 	alert("Clicking thsi would take you to the \""+ $(this).attr("title")+ "\" project.");
-})
-
+});
 
 	
 })();
